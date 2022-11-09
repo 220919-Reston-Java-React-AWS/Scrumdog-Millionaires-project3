78f2d5e3a2c92043fd917b8c78a655cf89c818c8
hgarcia8889 <henry382@revature.net>
2022-11-09 15:09:24 -0500

    Starting direct messaging functionality in the backend.

---
+++ social-media-spring-main/src/main/java/com/revature/repositories/DMRepository.java
@@ -0,0 +1,7 @@
+package com.revature.repositories;
+
+import com.revature.models.DM;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DMRepository extends JpaRepository<DM, Integer> {
+}
