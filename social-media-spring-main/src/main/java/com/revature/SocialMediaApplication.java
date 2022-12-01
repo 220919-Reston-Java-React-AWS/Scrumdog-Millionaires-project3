2619696422815730f0fca890169f7020c262088a
Opale <opale.m.schappert@gmail.com>
2022-12-01 10:11:09 -0700

    New CORS Bean

--- social-media-spring-main/src/main/java/com/revature/SocialMediaApplication.java
+++ social-media-spring-main/src/main/java/com/revature/SocialMediaApplication.java
@@ -2,12 +2,24 @@ package com.revature;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SocialMediaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SocialMediaApplication.class, args);
+	}
+	@Bean
+	public WebMvcConfigurer configure() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://travelogfe.s3-website-us-east-1.amazonaws.com/").allowCredentials(true).allowedMethods("OPTIONS", "PUT", "POST", "PATCH", "DELETE", "GET");
+			}
+		};
 	}
 
 }
